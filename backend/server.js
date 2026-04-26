f6f08d1d395174c8c36248212923b04a5382830b
amitihere <amiti.aneesh1@gmail.com>
2026-04-26 11:49:34 +0530

    email validate

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
